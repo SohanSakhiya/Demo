1816beb4bb098540e96853c394c56ff51443bd3c
Sohan Sakhiya <sohansakhiya5201@gmail.com>
2021-09-07 15:05:46 +0530

    modifying sum.java

--- sum.java
+++ sum.java
@@ -3,6 +3,7 @@ public class sum {
     public static void main(String[] args) {
         int a=5 ,b= 10;
         System.out.println("Sum Of A and B is " + (a+b));
+        System.out.println("2 Sum Of A and B is " + (a+b));
 
     }
 
