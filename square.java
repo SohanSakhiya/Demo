90682a8fcb4b26344e1f422b35a480f6d72456ab
Sohan Sakhiya <sohansakhiya5201@gmail.com>
2021-09-07 15:54:27 +0530

    adding square.java

---
+++ square.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class square {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println("Square of "+ "Is" + (a*a));
+        sc.close();
+    }
+
+}
