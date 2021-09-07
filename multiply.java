e570be29b3a5bf3a102473dc6a203ea9b85ba197
Sohan Sakhiya <sohansakhiya5201@gmail.com>
2021-09-07 15:21:11 +0530

    Added multiply.java

---
+++ multiply.java
@@ -0,0 +1,8 @@
+public class multiply {
+
+        public static void main(String[] args) {
+            int a=5 ,b= 10;
+            System.out.println("Sum Of A and B is " + (a*b));
+        }
+    
+    }
